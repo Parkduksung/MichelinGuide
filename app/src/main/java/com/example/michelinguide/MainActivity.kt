0d32f9cb9bfec6352d3ea44dc1a446e0730ec88f
Parkduksung <duksung1234@naver.com>
2021-12-16 00:01:50 +0900

    [feature] 불필요한 구문 제거.

--- app/src/main/java/com/example/michelinguide/MainActivity.kt
+++ app/src/main/java/com/example/michelinguide/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.michelinguide
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.util.Base64
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
@@ -14,8 +10,6 @@ import com.example.basemaplib.module.fragment.MapProvider
 import com.example.michelinguide.databinding.ActivityMainBinding
 import com.example.michelinlib.factory.MapProviderFactory
 import com.example.michelinlib.factory.MapType
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
 
 class MainActivity : AppCompatActivity() {
 
@@ -64,27 +58,6 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == 999) {
             supportFragmentManager.beginTransaction()
                 .replace(binding.containerMap.id, mapProvider.getMapFragment()).commit()
-        }
-    }
-
-    @SuppressLint("PackageManagerGetSignatures")
-    private fun getHashKey() {
-        var packageInfo: PackageInfo? = null
-        try {
-            packageInfo =
-                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
-        } catch (e: PackageManager.NameNotFoundException) {
-            e.printStackTrace()
-        }
-        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
-        for (signature in packageInfo!!.signatures) {
-            try {
-                val md: MessageDigest = MessageDigest.getInstance("SHA")
-                md.update(signature.toByteArray())
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
-            } catch (e: NoSuchAlgorithmException) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
-            }
         }
     }
 
