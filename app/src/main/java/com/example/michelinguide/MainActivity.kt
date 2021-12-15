3aca027678c804679f30105570c360a681da3d88
Parkduksung <duksung1234@naver.com>
2021-12-15 22:49:20 +0900

    [feature]app 에 적용.

--- app/src/main/java/com/example/michelinguide/MainActivity.kt
+++ app/src/main/java/com/example/michelinguide/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.michelinguide
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.basemaplib.module.fragment.MapProvider
+import com.example.michelinlib.factory.MapProviderFactory
+import com.example.michelinlib.factory.MapType
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mapProvider: MapProvider
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapProvider = MapProviderFactory.create(MapType.KAKAO)
+
     }
+
+
 }
