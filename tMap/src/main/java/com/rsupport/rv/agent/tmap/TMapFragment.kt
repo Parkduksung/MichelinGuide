1b322a0285678329667aebaa473cc4e40f085e57
Parkduksung <duksung1234@naver.com>
2021-12-20 23:27:09 +0900

    [feature] tmap 경로 추가.

--- tMap/src/main/java/com/rsupport/rv/agent/tmap/TMapFragment.kt
+++ tMap/src/main/java/com/rsupport/rv/agent/tmap/TMapFragment.kt
@@ -1,14 +1,19 @@
 package com.rsupport.rv.agent.tmap
 
 
-import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import com.example.basemaplib.module.base.BaseFragment
 import com.rsupport.rv.agent.tmap.databinding.FragmentTmapBinding
+import com.skt.Tmap.TMapData
 import com.skt.Tmap.TMapMarkerItem
 import com.skt.Tmap.TMapPoint
 import com.skt.Tmap.TMapView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class TMapFragment : BaseFragment<FragmentTmapBinding>(R.layout.fragment_tmap) {
@@ -19,23 +24,47 @@ class TMapFragment : BaseFragment<FragmentTmapBinding>(R.layout.fragment_tmap) {
         }
     }
 
-    private fun addMarker(tMapPoint: TMapPoint, name: String) {
+    private val markerA =
+        Marker(name = "서울시청", mapPoint = TMapPoint(37.5670135, 126.9783740))
+    private val markerB =
+        Marker(name = "노원역", mapPoint = TMapPoint(37.6563403513278, 127.063449137455))
+
+    private fun addMarker(marker: Marker) {
         val tMapMarkerItem = TMapMarkerItem().apply {
             setPosition(0.5f, 1.0f)
-            this.tMapPoint = tMapPoint
-            this.name = name
+            this.tMapPoint = marker.mapPoint
+            this.name = marker.name
         }
-        tmapView.addMarkerItem(name, tMapMarkerItem)
+        tmapView.addMarkerItem(marker.name, tMapMarkerItem)
     }
 
     override fun setMockMarker() {
-        addMarker(TMapPoint(37.5670135, 126.9783740), "서울시청")
-        addMarker(TMapPoint(37.6563403513278, 127.063449137455), "노원역")
+        addMarker(markerA)
+        addMarker(markerB)
         tmapView.setZoom(11.0f)
+        CoroutineScope(Dispatchers.IO).launch {
+            showRoute(markerA.mapPoint, markerB.mapPoint)
+        }
+    }
+
+    private suspend fun showRoute(start: TMapPoint, end: TMapPoint) = withContext(Dispatchers.IO) {
+        try {
+            val tMapPolyLine = TMapData().findPathData(start, end)
+            tMapPolyLine.lineColor = Color.BLUE
+            tMapPolyLine.lineWidth = 10f
+            tmapView.addTMapPolyLine("tMapPolyLine", tMapPolyLine)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.containerTmap.addView(tmapView)
     }
+
+    data class Marker(
+        val name: String,
+        val mapPoint: TMapPoint
+    )
 }
