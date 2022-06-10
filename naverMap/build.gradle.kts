a69a48f0b2bac20870e72e6b347ddf2d810f2c43
Parkduksung <duksung1234@naver.com>
2022-06-10 16:40:56 +0900

    [feature] 마이그레이션 dsl - naver

---
+++ naverMap/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+
+
+android {
+    compileSdkVersion(AppConfig.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(AppConfig.minSdk)
+        targetSdkVersion(AppConfig.targetSdk)
+
+        testInstrumentationRunner = AppConfig.androidTestInstrumentation
+        consumerProguardFiles(AppConfig.proguardConsumerRules)
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        getByName("debug"){
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(project(":basemaplib"))
+
+    //app libs
+    implementation(AppDependencies.appLibraries)
+
+    //test lib
+    testImplementation(AppDependencies.testLibraries)
+    androidTestImplementation(AppDependencies.androidTestLibraries)
+
+    implementation("com.naver.maps:map-sdk:3.13.0")
+    implementation("com.google.android.gms:play-services-location:19.0.0")
+
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+}
