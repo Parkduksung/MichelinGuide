db008f9a73a18093ad3a6157be14ef88da22f58f
Parkduksung <duksung1234@naver.com>
2022-06-10 16:48:26 +0900

    [feature] 마이그레이션 dsl - michelin 추가.

---
+++ michelinlib/build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+
+
+android {
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+
+        testInstrumentationRunner = AppConfig.androidTestInstrumentation
+        consumerProguardFiles(AppConfig.proguardConsumerRules)
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        getByName("debug"){
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(project(":basemaplib"))
+    implementation(project(":naverMap"))
+    implementation(project(":kakaoMap"))
+    implementation(project(":tMap"))
+
+    //app libs
+    implementation(AppDependencies.appLibraries)
+
+    //test lib
+    testImplementation(AppDependencies.testLibraries)
+    androidTestImplementation(AppDependencies.androidTestLibraries)
+}
