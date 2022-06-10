587ee81d96167388b36034fc782dea0d6d6da3d9
Parkduksung <duksung1234@naver.com>
2022-06-10 16:37:45 +0900

    [feature] 마이그레이션 dsl - tmap

---
+++ tMap/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+
+
+android {
+    compileSdkVersion(AppConfig.compileSdk)
+
+    defaultConfig {
+        minSdkVersion(AppConfig.minSdk)
+        targetSdkVersion(AppConfig.targetSdk)
+
+        testInstrumentationRunner = AppConfig.androidTestInstrumentation
+        consumerProguardFiles(AppConfig.proguardConsumerRules)
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+
+        getByName("debug"){
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(project(":basemaplib"))
+    implementation(files("libs/com.skt.Tmap_1.70.jar"))
+
+    //app libs
+    implementation(AppDependencies.appLibraries)
+
+    //test lib
+    testImplementation(AppDependencies.testLibraries)
+    androidTestImplementation(AppDependencies.androidTestLibraries)
+}
